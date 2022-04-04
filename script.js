a9dfaa0dd6a12c85ef775908fcd8c1ad17495654
KaylaneMoreira <kaylanenim@gmail.com>
2022-04-04 20:40:49 -0300

    Update

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
+let show = true;
 const toggleButton = document.getElementsByClassName('toggle-button')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
+  document.body.style.overflow = show ? "hidden" : "initial"
+  navbarLinks.classList.toggle('active', show)
+  show = !show;
+  
 })
